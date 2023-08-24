5c149018cc464b959e0914b4674adb48cd742268
ASUS <you@example.com>
2023-08-24 21:02:00 +0300

    Added Mercy Component

---
+++ src/Mercy.js
@@ -0,0 +1,11 @@
+function Mercy(){
+    return(
+        <div>
+            <h5>A little about me</h5>
+            <p>Hello there, my name is Mercy Muriithi. Welcome to knowing me.</p>
+            <img src="https://images.pexels.com/photos/102061/pexels-photo-102061.jpeg?auto=compress&cs=tinysrgb&w=300" alt="coding@best" />
+            <p>Hi, I'm [Your Name], a student hooked on coding with JavaScript. I'm all about building web apps – from dynamic interfaces to interactive features. Turning lines of code into cool web experiences is my thing. When I'm not coding, you'll find me solving puzzles and teaming up with others. Let's dive into the world of web tech together!</p>
+        </div>
+    )
+}
+export default Mercy;
