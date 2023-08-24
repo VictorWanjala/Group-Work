5e3fd7e1b650d07c363ecf3fba970ec30fc0642c
williamNyongesa <marakawilliam@gmail.com>
2023-08-24 21:06:35 +0300

    add william component

---
+++ src/William.js
@@ -0,0 +1,9 @@
+const William = () => {
+    return ( 
+        <div>
+            I'm William, a coding enthusiast who's captivated by the world of programming. As an ardent fan of technology, I find joy in crafting elegant solutions to complex problems. Beyond the screen, I have a deep appreciation for connecting with people and cherish the beauty of human interactions.
+        </div>
+     );
+}
+ 
+export default William;
